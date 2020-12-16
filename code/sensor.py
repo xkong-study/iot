e8f604e13245915bff7c1c40db6ad37700c57888
Basil L. Contovounesios <contovob@tcd.ie>
2020-12-16 17:37:17 +0000

    PEP8-ify sensor.py a bit

--- code/sensor.py
+++ code/sensor.py
@@ -8,19 +8,24 @@ import time
 import types
 
 
-class sensor:
+class Sensor:
+    # Map sensor types to random value generator functions
+    generators = {'speed': lambda: random.randint(40, 90),
+                  'proximity': lambda: random.randint(1, 50),
+                  'pressure': lambda: random.randint(20, 40),
+                  'heartrate': lambda: random.randint(40, 120)}
+
     def __init__(self, host, port):
         self.host = host
         self.port = port
         self.selector = selectors.DefaultSelector()
         self.finished = False
 
-
     def start_connections(self, num_conns, messages):
         server_addr = (self.host, self.port)
         for i in range(0, num_conns):
             conn_id = i + 1
-            print("Starting connection #", conn_id, "to", server_addr)
+            print(f'Starting connection #{conn_id} to {server_addr}')
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             sock.setblocking(False)
             sock.connect_ex(server_addr)
@@ -32,31 +37,26 @@ class sensor:
                                          out_bytes='')
             self.selector.register(sock, events, data=data)
 
-    def service_connection(self, key, mask,sensorType):
+    def service_connection(self, key, mask, sensorType):
         sock = key.fileobj
         data = key.data
         if mask & selectors.EVENT_READ:
             recv_data = sock.recv(1024)
             if recv_data:
-                #print('Received', repr(recv_data), 'from connection', data.conn_id)
+                # print('Received', repr(recv_data),
+                #       'from connection', data.conn_id)
                 data.recv_total += len(recv_data)
-            #if not recv_data or data.recv_total == data.msg_total:
-             #   print('Closing connection #', data.conn_id)
-              #  self.finished = True
-               # self.selector.unregister(sock)
-                #sock.close()
+            # if not recv_data or data.recv_total == data.msg_total:
+            #     print(f'Closing connection #{data.conn_id}')
+            #     self.finished = True
+            #     self.selector.unregister(sock)
+            #     sock.close()
         elif mask & selectors.EVENT_WRITE:
             if not data.out_bytes and data.messages:
                 data.out_bytes = data.messages.pop(0)
-                if sensorType == 'speed':
-                    d = sensorType + " " + str(random.randint(40, 90))
-                elif sensorType == 'proximity':
-                    d = sensorType + " " + str(random.randint(1, 50))
-                elif sensorType == 'pressure':
-                    d = sensorType + " " + str(random.randint(20, 40))
-                elif sensorType == 'heartrate':
-                    d = sensorType + " " + str(random.randint(40, 120))
-                data.messages.append(d.encode('utf-8'))
+                val = Sensor.generators.get(sensorType, lambda: None)()
+                valstr = f'{sensorType} {val}'
+                data.messages.append(valstr.encode('utf-8'))
             if data.out_bytes:
                 print(data.out_bytes.decode('utf-8'))
                 sent = sock.send(data.out_bytes)
@@ -64,17 +64,16 @@ class sensor:
                 time.sleep(5)
         return self.finished
 
-    def run(self,sensorType):
-        i = 0
-        while self.finished == False:
+    def run(self, sensorType):
+        while not self.finished:
             events = self.selector.select(timeout=None)
             for key, mask in events:
                 try:
-                    self.service_connection(key, mask,sensorType)
+                    self.service_connection(key, mask, sensorType)
                 except ConnectionRefusedError:
-                    print('Host node with address {Host:', self.host, ', Port:', self.port, '} not found')
+                    addr = f'{{Host:{self.host}, Port:{self.port}}}'
+                    print(f'Node with address {addr} not found')
                     break
-                i += 1
 
 
 def main():
@@ -83,18 +82,15 @@ def main():
     args = parser.parse_args()
     sensorType = args.sensortype
 
-    PORT_TABLE = {'VehiclePort': 33401}         # Hardcoded port table for testing
-    PORT = PORT_TABLE['VehiclePort']            # The port used by the server
+    port_table = {'VehiclePort': 33401}  # Hardcoded port table for testing
+    port = port_table['VehiclePort']     # The port used by the server
 
     initial_message = sensorType + " 1"
     byte_messages = [initial_message.encode('UTF-8')]
-    # data = {}
-    # data["key"] = "value"
-    # json_data = json.dumps(data)
 
     # The server's hostname or IP address
     host = socket.gethostbyname(socket.gethostname())
-    mySensor = sensor(host, PORT)
+    mySensor = Sensor(host, port)
     mySensor.start_connections(1, byte_messages)
     mySensor.run(sensorType)
 
