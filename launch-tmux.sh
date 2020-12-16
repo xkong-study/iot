80b0e7966e669249344f194474d94d5635e582d8
Basil L. Contovounesios <contovob@tcd.ie>
2020-12-16 17:15:15 +0000

    Improve script documentation and flexibility

--- launch-tmux.sh
+++ launch-tmux.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # launch-tmux.sh - run V2V, V2I, and sensors in multiple tmux panes
 
+# Careful now
 set -o errexit
 set -o nounset
 
@@ -12,16 +13,23 @@ usage() {
   echo 'Options:'
   echo
   echo '  -h, ?       Print this help text'
-  echo '  -i          Run V2I as well as V2v'
+  echo '  -i          Run V2I as well as V2V'
+  echo '  -d DELAY    Seconds to wait before spawning sensors, which'
+  echo '              expect a vehicle to already be running.'
+  echo '              (default: 4)'
   exit 1
 }
 
-while getopts ih f; do
+while getopts d:ih f; do
   case "$f" in
+    d) delay="${OPTARG}";;
     i) infra=true;;
     h | \?) usage;;
   esac
 done
+delay="${delay:-4}"
+
+# DWIM (not what I say) venv creation and activation
 
 if [ ! -d .venv ]; then
   echo 'No .venv directory found; creating'
@@ -35,22 +43,28 @@ elif [ ! -x "$(command -v deactivate)" ]; then
   . .venv/bin/activate
 fi
 
+# Fire the missiles
+
 if [ -z "${infra:-}" ]; then
-  tmux new 'python code/peerTry.py'                                 \; \
-       splitw    'sleep 4; ./code/sensor.py --sensortype=speed'     \; \
-       splitw    'sleep 4; ./code/sensor.py --sensortype=proximity' \; \
-       splitw -h 'sleep 4; ./code/sensor.py --sensortype=heartrate' \; \
-       selectp -U                                                   \; \
-       splitw -h 'sleep 4; ./code/sensor.py --sensortype=pressure'
+  # No infrastructure node: just a vehicle and its trusty sensors
+  tmux new './code/peerTry.py'                                             \; \
+       splitw    "sleep ${delay}; ./code/sensor.py --sensortype=speed"     \; \
+       splitw    "sleep ${delay}; ./code/sensor.py --sensortype=proximity" \; \
+       splitw -h "sleep ${delay}; ./code/sensor.py --sensortype=heartrate" \; \
+       selectp -U                                                          \; \
+       splitw -h "sleep ${delay}; ./code/sensor.py --sensortype=pressure"
 else
-  tmux new './code/infra.py'                                        \; \
-       splitw -h 'python code/peerTry.py'                           \; \
-       splitw    'sleep 4; ./code/sensor.py --sensortype=speed'     \; \
-       splitw    'sleep 4; ./code/sensor.py --sensortype=proximity' \; \
-       selectp -L                                                   \; \
-       splitw    'sleep 4; ./code/sensor.py --sensortype=heartrate' \; \
-       splitw    'sleep 4; ./code/sensor.py --sensortype=pressure'
+  # Infrastructure and vehicle
+  tmux new './code/infra.py'                                               \; \
+       splitw -h './code/peerTry.py'                                       \; \
+       splitw    "sleep ${delay}; ./code/sensor.py --sensortype=speed"     \; \
+       splitw    "sleep ${delay}; ./code/sensor.py --sensortype=proximity" \; \
+       selectp -L                                                          \; \
+       splitw    "sleep ${delay}; ./code/sensor.py --sensortype=heartrate" \; \
+       splitw    "sleep ${delay}; ./code/sensor.py --sensortype=pressure"
 fi
+
+# Be a good netizen
 
 echo 'Deactivating .venv'
 deactivate
